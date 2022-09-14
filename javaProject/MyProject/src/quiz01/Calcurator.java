89df6921be31047e5b936cda493de9c02709db07
Seo-Aram <arams.office@gmail.com>
2022-09-14 12:13:30 +0900

    Initial commit

---
+++ javaProject/MyProject/src/quiz01/Calcurator.java
@@ -0,0 +1,29 @@
+package quiz01;
+
+public class Calcurator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static long sum(int a, int b) {
+		long result = a + b;
+		return result;
+	}
+	
+	public static long sub(int a, int b) { 
+		long result = a - b;
+		return result;
+	}
+	
+	public static long mul(int a, int b) {
+		long result = a * b;
+		return result;
+	}
+	
+	public static double div(int a, int b) {
+		double result = a / b;
+		return result;
+	}
+}
---
+++ javaProject/MyProject/src/quiz01/Person.java
@@ -0,0 +1,10 @@
+package quiz01;
+
+public class Person {
+	public static void main(String[] args) {
+		String name = "Seo Aram";
+		String phone = "010-0000-0000";
+		String rrn = "000000-0000000";
+	}
+
+}
