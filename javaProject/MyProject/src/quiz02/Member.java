724e7666dd1b5dc13ecbf1f78f16456684c39164
Seo-Aram <arams.office@gmail.com>
2022-09-16 12:27:20 +0900

    nowYear추가
    LocalDate.now().getYear() 구문 위치 변경

--- javaProject/MyProject/src/quiz02/Member.java
+++ javaProject/MyProject/src/quiz02/Member.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Member {
+	
+	static int nowYear = LocalDate.now().getYear();
 
 	public static void printName(String name) {
 		System.out.println("안녕하세요! " + name + " 입니다.");
@@ -26,25 +28,25 @@ public class Member {
 	public static void printCheckFreeVaccine(int year) {
 		//나이를 계산해야 함
 		int age = getAge(year);
-		if(age < 15 || 65 <= age) 
+		if(age < 15 || 65 <= age) {
 			System.out.println("무료예방접종이 가능합니다.");
-		else
+		} else {
 			System.out.println("무료접종 대상이 아닙니다.");
+		}
 	}
 	
 	public static void printCheckMedical(int year) {
 		int age = getAge(year);
-		if(20 <= age && (year + LocalDate.now().getYear()) % 2 == 0) {
-			System.out.println("건강 검진 대상입니다.");
-			if(40 <= age) 
-				System.out.println("암 검사 대상입니다.");
-		} else {
-			System.out.println("건강 검진 대상이 아닙니다.");
+		if(20 <= age && (year + nowYear) % 2 == 0) {
+			System.out.println("무료 건강 검진 대상입니다.");
+			if(40 <= age) {
+				System.out.println("무료 암 검사 대상입니다.");
+			}
 		}
 	}
 	
 	public static int getAge(int year) {
-		return LocalDate.now().getYear() - year;
+		return nowYear - year;
 	}
 	
 }
