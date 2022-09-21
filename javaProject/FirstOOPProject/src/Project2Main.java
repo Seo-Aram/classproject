902d73d9d4a84dc10efd791f88aae697105d7abe
Seo-Aram <arams.office@gmail.com>
2022-09-21 14:13:39 +0900

    미니 프로젝트 2

---
+++ javaProject/FirstOOPProject/src/Project2Main.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+
+import data.Contact;
+import data.SmartPhone;
+
+public class Project2Main {
+	public static void main(String[] args) {
+		SmartPhone phoneData = new SmartPhone();
+		Scanner in = new Scanner(System.in);
+		int input;
+		String name, phoneNumber, email, address, group;
+		int birth;
+		
+		while(true) {
+			System.out.println("======= 연락처 =======");
+			System.out.println("1. 입력");
+			System.out.println("2. 수정");
+			System.out.println("3. 검색");
+			System.out.println("4. 삭제");
+			System.out.println("5. 출력");
+			System.out.println();
+			System.out.println("0. 종료");
+			System.out.println("=======      =======");
+			System.out.print("원하시는 항목을 입력하세요 >> ");
+			input = in.nextInt();
+			in.nextLine();
+			
+			switch(input) {
+				case 1: // 입력
+				{
+					System.out.println("연락처 입력");
+					System.out.print("이름>>");
+					name = in.nextLine();
+					System.out.print("전화번호>>");
+					phoneNumber = in.nextLine();
+					System.out.print("e-Mail>>");
+					email = in.nextLine();
+					System.out.print("주소>>");
+					address = in.nextLine();
+					System.out.print("생일>>");
+					birth = in.nextInt();
+					in.nextLine();
+					System.out.print("그룹>>");
+					group = in.nextLine();
+					
+					phoneData.insertContactData(name, phoneNumber, email, address, birth, group);
+					
+					break;
+				}
+				case 2: // 수정
+				{
+					System.out.println("연락처 수정");
+					System.out.print("수정할 연락처 이름>>");
+					name = in.nextLine();
+					
+					int index = phoneData.findContactIndexForName(name);
+					if(index == -1) {
+						System.out.println("해당 데이터는 존재하지 않습니다.");
+						continue;
+					}
+					
+					System.out.print("이름>>");
+					name = in.nextLine();
+					System.out.print("전화번호>>");
+					phoneNumber = in.nextLine();
+					System.out.print("e-Mail>>");
+					email = in.nextLine();
+					System.out.print("주소>>");
+					address = in.nextLine();
+					System.out.print("생일>>");
+					birth = in.nextInt();
+					in.nextLine();
+					System.out.print("그룹>>");
+					group = in.nextLine();
+					
+					phoneData.updateContactDataForIndex(index, name, phoneNumber, email, address, birth, group);
+					
+					break;
+				}
+				case 3: // 검색
+				{
+					System.out.print("검색하고자 하는 이름을 입력하세요>>");
+					name = in.nextLine();
+					
+					Contact data = phoneData.findContactDataForName(name);
+					if(data == null ) {
+						System.out.println("데이터가 존재하지 않습니다.");
+					} else {
+						data.printData();
+					}
+					
+					break;
+				}
+				case 4: // 삭제
+				{
+					System.out.print("삭제할 연락처의 이름을 입력하세요>>");
+					name = in.nextLine();
+					
+					phoneData.deleteContactDataForName(name);
+					
+					break;
+				}
+				case 5: // 출력
+				{
+					System.out.println("연락처 내역 전체 출력");
+					phoneData.printContactAllData();
+					break;
+				}
+				case 0:
+				{
+					System.out.println("연락처를 종료합니다.");
+					in.close();
+					return;
+				}
+				default:
+				{
+					System.out.println("잘못 입력했습니다.");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ javaProject/FirstOOPProject/src/data/SmartPhone.java
@@ -0,0 +1,86 @@
+package data;
+
+public class SmartPhone {
+	private Contact[] list;
+	private final int MAX_SIZE = 10;
+	
+	public SmartPhone() {
+		list = new Contact[MAX_SIZE];
+	}
+
+	public Contact getContactDataForIndex(int index) {
+		if(index < 0 || list.length <= index) {
+			return null;
+		}
+		return list[index];
+	}
+	
+	public void updateContactDataForIndex(int index, String name, String phoneNumber
+			, String eMail, String address, int birth, String group) {
+		if(list[index] == null) {
+			System.out.println("데이터가 존재하지 않습니다.");
+		} else {
+			list[index].setName(name);
+			list[index].setPhoneNumber(phoneNumber);
+			list[index].seteMail(eMail);
+			list[index].setAddress(address);
+			list[index].setBirth(birth);
+			list[index].setGroup(group);
+		}
+	}
+	
+	public void insertContactData(String name, String phoneNumber
+			, String eMail, String address, int birth, String group) {
+		for(int i = 0; i < MAX_SIZE; ++i) {
+			if(list[i] == null) {
+				list[i] = new Contact(name, phoneNumber, eMail, address, birth, group);
+				System.out.println("데이터 입력 완료");
+				return;
+			}
+		}
+		System.out.println("연락처가 가득 찼습니다.");
+	}
+	
+	public Contact findContactDataForName(String name) {
+		for(Contact data : list) {
+			if(data != null && data.getName().equals(name)) {
+				return data;
+			}
+		}
+		
+		return null;
+	}
+	
+	public int findContactIndexForName(String name) {
+		for(int i = 0; i < MAX_SIZE; ++i) {
+			if(list[i] != null) {
+				if(list[i].getName().equals(name)) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+	
+	public void deleteContactDataForName(String name) {
+		for(int i = 0; i < MAX_SIZE; ++i) {
+			if(list[i] != null) {
+				if(list[i].getName().equals(name)) {
+					list[i] = null;
+					System.out.println("데이터 삭제 완료.");
+					return;
+				}
+			}
+		}
+		
+		System.out.println("올바른 데이터를 찾지 못했습니다.");
+	}
+	
+	public void printContactAllData() {
+		for(Contact data : list) {
+			if(data != null) {
+				data.printData();
+			}
+		}
+	}
+}
