ec9c758f649e2383b96e9479188c39aa4d41135a
Seo-Aram <arams.office@gmail.com>
2022-09-22 14:38:16 +0900

    공통 사용 구문 함수로 변경

--- javaProject/FirstOOPProject/src/step02/Project2Main.java
+++ javaProject/FirstOOPProject/src/step02/Project2Main.java
@@ -1,24 +1,19 @@
 package step02;
 import java.util.Scanner;
 
+enum MENU {CLOSE, INSERT, UPDATE, SEARCH, DELETE, PRINT_ALL};
+
 public class Project2Main {
+	static Scanner in = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		SmartPhone phoneData = SmartPhone.getInstance();
-		Scanner in = new Scanner(System.in);
 		int input;
-		String name, phoneNumber, email, address, group, birth;
+		String name;
 		
 		while(true) {
-			System.out.println("======= 연락처 =======");
-			System.out.println("1. 입력");
-			System.out.println("2. 수정");
-			System.out.println("3. 검색");
-			System.out.println("4. 삭제");
-			System.out.println("5. 출력");
-			System.out.println();
-			System.out.println("0. 종료");
-			System.out.println("=======      =======");
-			System.out.print("원하시는 항목을 입력하세요 >> ");
+			printMenu();
+			
 			input = in.nextInt();
 			in.nextLine();
 			
@@ -26,26 +21,15 @@ public class Project2Main {
 				case 1: // 입력
 				{
 					System.out.println("연락처 입력");
-					System.out.print("이름>>");
-					name = in.nextLine();
-					System.out.print("전화번호>>");
-					phoneNumber = in.nextLine();
-					System.out.print("e-Mail>>");
-					email = in.nextLine();
-					System.out.print("주소>>");
-					address = in.nextLine();
-					System.out.print("생일>>");
-					birth = in.nextLine();
-					System.out.print("그룹>>");
-					group = in.nextLine();
 					
-					phoneData.insertContactData(name, phoneNumber, email, address, birth, group);
+					insertUpdateContact(MENU.INSERT, -1);
 					
 					break;
 				}
 				case 2: // 수정
 				{
 					System.out.println("연락처 수정");
+					
 					System.out.print("수정할 연락처 이름>>");
 					name = in.nextLine();
 					
@@ -55,22 +39,9 @@ public class Project2Main {
 						continue;
 					}
 					System.out.println("수정할 데이터가 없을 경우 공백으로 남기시면 수정되지 않습니다.");
+					
+					insertUpdateContact(MENU.UPDATE, index);
 
-					System.out.print("이름>>");
-					name = in.nextLine();
-					System.out.print("전화번호>>");
-					phoneNumber = in.nextLine();
-					System.out.print("e-Mail>>");
-					email = in.nextLine();
-					System.out.print("주소>>");
-					address = in.nextLine();
-					System.out.print("생일>>");
-					birth = in.nextLine();
-					System.out.print("그룹>>");
-					group = in.nextLine();
-					
-					phoneData.updateContactDataForIndex(index, name, phoneNumber, email, address, birth, group);
-					
 					break;
 				}
 				case 3: // 검색
@@ -115,5 +86,45 @@ public class Project2Main {
 			}
 			System.out.println();
 		}
+	}
+	
+	static void printMenu() {
+		System.out.println("======= 연락처 =======");
+		System.out.println("1. 입력");
+		System.out.println("2. 수정");
+		System.out.println("3. 검색");
+		System.out.println("4. 삭제");
+		System.out.println("5. 출력");
+		System.out.println();
+		System.out.println("0. 종료");
+		System.out.println("=======      =======");
+		System.out.print("원하시는 항목을 입력하세요 >> ");	
+	}
+	
+	static void insertUpdateContact(MENU type, int index) {
+		SmartPhone phoneData = SmartPhone.getInstance();
+		
+		String name, phoneNumber, email, address, group, birth;
+		
+		System.out.print("이름>>");
+		name = in.nextLine();
+		System.out.print("전화번호>>");
+		phoneNumber = in.nextLine();
+		System.out.print("e-Mail>>");
+		email = in.nextLine();
+		System.out.print("주소>>");
+		address = in.nextLine();
+		System.out.print("생일>>");
+		birth = in.nextLine();
+		System.out.print("그룹>>");
+		group = in.nextLine();
+		
+
+		if(type == MENU.UPDATE) {
+			phoneData.updateContactDataForIndex(index, name, phoneNumber, email, address, birth, group);
+		} else if(type == MENU.INSERT) {
+			phoneData.insertContactData(name, phoneNumber, email, address, birth, group);
+		}
+		
 	}
 }
