7c5da12f0ae133d390ce8a15e562b87f1dcebf21
Seo-Aram <arams.office@gmail.com>
2022-09-22 15:03:30 +0900

    주석 추가

--- javaProject/FirstOOPProject/src/step02/SmartPhone.java
+++ javaProject/FirstOOPProject/src/step02/SmartPhone.java
@@ -10,6 +10,10 @@ public class SmartPhone {
 		list = new Contact[MAX_SIZE];
 	}
 	
+	/**
+	 * 싱글톤 객체
+	 * @return SmartPhone
+	 */
 	public static SmartPhone getInstance() { // 싱글톤 객체
 		if(instance == null) {
 			instance = new SmartPhone();
@@ -17,6 +21,11 @@ public class SmartPhone {
 		return instance;
 	}
 
+	/**
+	 * index에 위치한 데이터를 반환하느 함수
+	 * @param index
+	 * @return Contact
+	 */
 	public Contact getContactDataForIndex(int index) {
 		if(index < 0 || list.length <= index) {
 			return null;
@@ -24,6 +33,16 @@ public class SmartPhone {
 		return list[index];
 	}
 	
+	/**
+	 * 데이터 업데이트 함수
+	 * @param index
+	 * @param name
+	 * @param phoneNumber
+	 * @param eMail
+	 * @param address
+	 * @param birth
+	 * @param group
+	 */
 	public void updateContactDataForIndex(int index, String name, String phoneNumber
 			, String eMail, String address, String birth, String group) {
 		if(index < 0 || MAX_SIZE <= index || list[index] == null) {
@@ -55,6 +74,15 @@ public class SmartPhone {
 		}
 	}
 	
+	/**
+	 * 데이터 입력 함수
+	 * @param name
+	 * @param phoneNumber
+	 * @param eMail
+	 * @param address
+	 * @param birth
+	 * @param group
+	 */
 	public void insertContactData(String name, String phoneNumber, 
 			String eMail, String address, String birth, String group) {
 		for(int i = 0; i < MAX_SIZE; ++i) {
@@ -67,6 +95,11 @@ public class SmartPhone {
 		System.out.println("연락처가 가득 찼습니다.");
 	}
 	
+	/**
+	 * name과 동일한 데이터를 반환하는 함수
+	 * @param name
+	 * @return Contact
+	 */
 	public Contact findContactDataForName(String name) {
 		for(Contact data : list) {
 			if(data != null && data.getName().equals(name)) {
@@ -77,17 +110,24 @@ public class SmartPhone {
 		return null;
 	}
 	
+	/**
+	 * name과 동일한 데이터의 index를 반환하는 함수
+	 * @param name
+	 * @return int index
+	 */
 	public int findContactIndexForName(String name) {
 		for(int i = 0; i < MAX_SIZE; ++i) {
-			if(list[i] != null) {
-				if(list[i].getName().equals(name)) {
-					return i;
-				}
+			if(list[i] != null && list[i].getName().equals(name)) {
+				return i;
 			}
 		}
 		return -1;
 	}
 	
+	/**
+	 * name과 동일한 데이터를 찾아 삭제하는 함수
+	 * @param name
+	 */
 	public void deleteContactDataForName(String name) {
 		int index = findContactIndexForName(name);
 		if(index != -1) {
@@ -99,6 +139,9 @@ public class SmartPhone {
 		System.out.println("올바른 데이터를 찾지 못했습니다.");
 	}
 	
+	/**
+	 * 모든 데이터 print
+	 */
 	public void printContactAllData() {
 		for(Contact data : list) {
 			if(data != null) {
