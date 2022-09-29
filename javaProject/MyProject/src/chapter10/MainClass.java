c88bc49dbc3016f4d49f2f76604662dfc17d2301
Seo-Aram <arams.office@gmail.com>
2022-09-29 10:23:19 +0900

    Update MainClass.java

--- javaProject/MyProject/src/chapter10/MainClass.java
+++ javaProject/MyProject/src/chapter10/MainClass.java
@@ -2,36 +2,33 @@ package chapter10;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class MainClass {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
 
 		System.out.print("입력하세요(영문 및 숫자)>>");
 		String str = scan.nextLine();
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9]*$");
 		
-		for(int i = 0; i < str.length(); ++i) {
-			char c = str.charAt(i);
-			try {
-				if( !(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9')) ) {
-					throw new BadIdInputException("입력 값은 영문 및 숫자만 가능합니다.");
-				}
-			} catch (BadIdInputException e) {
-				System.out.println(e.getMessage());
-				break;
+		try {
+			if(!pattern.matcher(str).find()) {
+				throw new BadIdInputException("입력 값은 영문 및 숫자만 가능합니다.");
 			}
+		} catch (BadIdInputException e) {
+			System.out.println(e.getMessage());
 		}
-
+		
 		System.out.print("입력하세요(숫자)>>");
 		try {
 			int result = scan.nextInt();
 			System.out.println("입력한 값은 " + result);
 		} catch (InputMismatchException e) {
 			System.out.println("데이터 입력이 잘못되었습니다.");
-			System.out.println(e.getMessage());
-			System.out.println(e.getLocalizedMessage());
 		}
 		
 		scan.close();
