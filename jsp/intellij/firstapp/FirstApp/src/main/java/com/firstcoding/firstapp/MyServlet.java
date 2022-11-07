13592dc5d746401fabb54515c3c50b9a098cc391
Seo-Aram <arams.office@gmail.com>
2022-11-07 18:00:32 +0900

    jsp/servlet

---
+++ jsp/intellij/firstapp/FirstApp/src/main/java/com/firstcoding/firstapp/MyServlet.java
@@ -0,0 +1,27 @@
+package com.firstcoding.firstapp;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="MyServlet", urlPatterns ="/my")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // super.doGet(request, response);
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>MyServlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>MyServlet</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
